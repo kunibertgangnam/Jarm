1cc79bbb29e0b996fe060fd1c49e42b674c2cc2a
Andreas <andreas.kuehn@ingentis.com>
2020-02-03 13:16:59 +0100

    Validierungsexception, User Klasse, Projectservie (leer)

--- jarm-main/src/main/java/interfaces/BenutzerService.java
+++
@@ -1,8 +0,0 @@
-package interfaces;
-
-public interface BenutzerService {
-
-	UserInterface userNew(String user, String password, String passwordRepeat);
-	
-	UserInterface userLogin(String user, String password);
-}
--- jarm-main/src/main/java/interfaces/ProjectService.java
+++ jarm-main/src/main/java/interfaces/ProjectService.java
@@ -2,14 +2,14 @@ package interfaces;
 
 public interface ProjectService {
 
-	void newProject(String projectname);
-	void loadMessages();
-	void loadUser();
-	void loadToDo();
-	void writeMessage();
-	void invite(String user);
-	void deletProject(String projectname);
-	void removeUser(String user);
-	void removeMessage();
-	void removeTodo();
+//	void newProject(String projectname);
+//	void loadMessages();
+//	void loadUser();
+//	void loadToDo();
+//	void writeMessage();
+//	void invite(String user);
+//	void deleteProject(String projectname);
+//	void removeUser(String user, String projectname);
+//	void removeMessage();
+//	void removeTodo();
 }
