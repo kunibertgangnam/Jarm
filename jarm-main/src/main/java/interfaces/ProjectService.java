0cc5a786e327dab58fab55e75bd86ef28d66f6b4
Andreas <andreas.kuehn@ingentis.com>
2020-02-03 12:05:11 +0100

    Projectinterface

--- jarm-main/src/main/java/interfaces/ProjectService.java
+++ jarm-main/src/main/java/interfaces/ProjectService.java
@@ -2,14 +2,14 @@ package interfaces;
 
 public interface ProjectService {
 
-	void newProject();
+	void newProject(String projectname);
 	void loadMessages();
 	void loadUser();
 	void loadToDo();
 	void writeMessage();
-	void invite();
-	void deletProject();
-	void removeUser();
+	void invite(String user);
+	void deletProject(String projectname);
+	void removeUser(String user);
 	void removeMessage();
 	void removeTodo();
 }
