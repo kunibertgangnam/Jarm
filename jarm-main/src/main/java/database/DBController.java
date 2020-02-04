b76fdd51d7e2182853ccfa7252582504aae062f0
Friedrich Kiltz <f@kiltz.de>
2020-02-04 11:28:42 +0100

    Erweiterung Build-Path

--- jarm-main/src/main/java/database/DBController.java
+++ jarm-main/src/main/java/database/DBController.java
@@ -2,57 +2,39 @@ package database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DBController {
-	
-    private static final DBController dbcontroller = new DBController();
-    //private static Connection connection;
-    private static final String DB_PATH = System.getProperty("user.home") + "\\" + "jarmdb.db";
 
-    static {
-        try {
-            Class.forName("org.sqlite.JDBC");
-        } catch (ClassNotFoundException e) {
-            System.err.println("Fehler beim Laden des JDBC-Treibers");
-            e.printStackTrace();
-        }
-    }
-    
-    private DBController(){
-    	initDBConnection();
-    }
-    
-    public static DBController getInstance(){
-        return dbcontroller;
-    }
-    
-    public Connection getConnection() {
-    	Connection connection;
-    	try {
-			connection = DriverManager.getConnection("jdbc:sqlite:" + DB_PATH);
-			return connection;
-		} catch (SQLException e) {
+	private static final DBController dbcontroller = new DBController();
+	// private static Connection connection;
+	private static final String DB_PATH = System.getProperty("user.home") + "/" + "jarmdb.db";
+
+	static {
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e) {
+			System.err.println("Fehler beim Laden des JDBC-Treibers");
 			e.printStackTrace();
 		}
-    	return null;
-    }
-    
-    private void initDBConnection() {
-        try {
-//            if (connection != null)
-//                return;
-//            System.out.println("Creating Connection to Database...");
-//            connection = DriverManager.getConnection("jdbc:sqlite:" + DB_PATH);
-//            if (!connection.isClosed())
-//                System.out.println("...Connection established");
-            
-            createTables();
-        } catch (SQLException e) {
-        	e.printStackTrace();
-        }
+	}
+
+	private DBController() {
+		initDBConnection();
+	}
+
+	public static DBController getInstance() {
+		return dbcontroller;
+	}
+
+	public Connection getConnection() throws SQLException {
+		return DriverManager.getConnection("jdbc:sqlite:" + DB_PATH);
+	}
+
+	private void initDBConnection() {
+
+		createTables();
 
 //        Runtime.getRuntime().addShutdownHook(new Thread() {
 //            public void run() {
@@ -67,10 +49,10 @@ public class DBController {
 //                }
 //            }
 //        });
-    }
-    
-    private void createTables() throws SQLException{
-    	try {
+	}
+
+	private void createTables() throws SQLException {
+		try {
 			Statement stmt = getConnection().createStatement();
 			stmt.execute(DBCreation.CREATE_USER_TABLE);
 			stmt.execute(DBCreation.CREATE_PROJECT_TABLE);
@@ -78,14 +60,14 @@ public class DBController {
 			stmt.execute(DBCreation.CREATE_PROJECT_TODO_TABLE);
 			stmt.execute(DBCreation.CREATE_PROJECT_TODO_USER_TABLE);
 			stmt.execute(DBCreation.CREATE_PROJECT_MESSAGE_TABLE);
-			
+
 			System.out.println("Tables created");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-    }
-    
-    public static void main(String[] args) {
+	}
+
+	public static void main(String[] args) {
 		System.out.println(DB_PATH);
 	}
 }
--- jarm-main/src/main/java/database/project/ProjectDAO.java
+++ jarm-main/src/main/java/database/project/ProjectDAO.java
@@ -20,38 +20,38 @@ import de.jarm.main.data.User;
 import utils.DateUtils;
 
 public class ProjectDAO {
-	
+
 	private Map<String, Project> projects;
-	
+
 	public void loadProjects() {
 		projects = new HashMap<String, Project>();
-		
-		try (Connection con = DBController.getInstance().getConnection();
-		         Statement stmt = con.createStatement()) {
 
-			ResultSet rs = stmt.executeQuery(DBStatements.SELECT_PROJECTS);				
+		try (Connection con = DBController.getInstance().getConnection(); Statement stmt = con.createStatement()) {
+
+			ResultSet rs = stmt.executeQuery(DBStatements.SELECT_PROJECTS);
 			while (rs.next()) {
-				int ownerId = rs.getInt("id");			
+				int ownerId = rs.getInt("id");
 				User u = getUserById(ownerId);
-				Project p = new Project(rs.getInt("id"), rs.getString("name"), u, DateUtils.toDate(rs.getString("created")));
+				Project p = new Project(rs.getInt("id"), rs.getString("name"), u,
+						DateUtils.toDate(rs.getString("created")));
 			}
-			
+
 			rs.close();
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void addProject(Project p, User u) {
-		
+
 		try (Connection con = DBController.getInstance().getConnection();
-		         PreparedStatement pstmt = con.prepareStatement(DBStatements.ADD_PROJECT)) {
-			
+				PreparedStatement pstmt = con.prepareStatement(DBStatements.ADD_PROJECT)) {
+
 			pstmt.setInt(0, u.getId());
 			pstmt.setString(1, DateUtils.toString(LocalDate.now()));
 			pstmt.setString(2, p.getTitle());
-			
+
 			pstmt.execute();
 //			ResultSet rs = stmt.executeQuery(DBStatements.ADD_PROJECT);				
 //			while (rs.next()) {
@@ -61,17 +61,17 @@ public class ProjectDAO {
 //			}
 //			
 //			rs.close();
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	private User getUserById(int userId) throws SQLException{
+
+	private User getUserById(int userId) throws SQLException {
 		try (Connection con = DBController.getInstance().getConnection();
-		         PreparedStatement pstmt = con.prepareStatement(DBStatements.SELECT_USER_BY_ID)) {
-			
+				PreparedStatement pstmt = con.prepareStatement(DBStatements.SELECT_USER_BY_ID)) {
+
 			pstmt.setInt(0, userId);
 			ResultSet rs = pstmt.executeQuery();
 			User u = new User(userId, rs.getString("name"), rs.getString("password"), rs.getString("email"));
@@ -82,92 +82,93 @@ public class ProjectDAO {
 		}
 		return null;
 	}
-	
-	private List<ProjectToDo> getProjectTodos(int projectId) throws SQLException{
+
+	private List<ProjectToDo> getProjectTodos(int projectId) throws SQLException {
 		List<ProjectToDo> todos = new ArrayList<>();
-		
+
 		try (Connection con = DBController.getInstance().getConnection();
-		         PreparedStatement pstmt = con.prepareStatement(DBStatements.SELECT_PROJECT_TODOS_BY_PROJECT_ID)) {
+				PreparedStatement pstmt = con.prepareStatement(DBStatements.SELECT_PROJECT_TODOS_BY_PROJECT_ID)) {
 			pstmt.setInt(0, projectId);
-			
+
 			ResultSet rs = pstmt.executeQuery();
-			
+
 			while (rs.next()) {
 				ProjectToDo d = new ProjectToDo(rs.getInt("id"), rs.getString("name"), rs.getString("description"));
 			}
-			
+
 			rs.close();
 		}
-		
-		
+
 		return todos;
 	}
-	
-	private List<User> getProjectTodoUsers(int todoId) throws SQLException{
+
+	private List<User> getProjectTodoUsers(int todoId) throws SQLException {
 		List<User> projectTodoUsers = new ArrayList<User>();
-		
+
 		try (Connection con = DBController.getInstance().getConnection();
-		         PreparedStatement pstmt = con.prepareStatement(DBStatements.SELECT_PROJECT_TODO_USERS_BY_PROJECT_TODO_ID)) {
-			
-		
+				PreparedStatement pstmt = con
+						.prepareStatement(DBStatements.SELECT_PROJECT_TODO_USERS_BY_PROJECT_TODO_ID)) {
+
 			pstmt.setInt(0, todoId);
 			ResultSet rs = pstmt.executeQuery();
-			
+
 			while (rs.next()) {
-				User u = new User(rs.getInt("id"), rs.getString("name"), rs.getString("password"), rs.getString("email"));
+				User u = new User(rs.getInt("id"), rs.getString("name"), rs.getString("password"),
+						rs.getString("email"));
 				projectTodoUsers.add(u);
 			}
 			rs.close();
-			return projectTodoUsers; 
+			return projectTodoUsers;
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
-	
-	private List<User> getProjectUsers(int projectId) throws SQLException{
+
+	private List<User> getProjectUsers(int projectId) throws SQLException {
 		List<User> projectUsers = new ArrayList<User>();
-		
+
 		try (Connection con = DBController.getInstance().getConnection();
-		         PreparedStatement pstmt = con.prepareStatement(DBStatements.SELECT_PROJECT_USERS_BY_PROJECT_ID)) {
+				PreparedStatement pstmt = con.prepareStatement(DBStatements.SELECT_PROJECT_USERS_BY_PROJECT_ID)) {
 			pstmt.setInt(0, projectId);
-			
-			ResultSet rs = pstmt.executeQuery();	
+
+			ResultSet rs = pstmt.executeQuery();
 			while (rs.next()) {
-				User u = new User(rs.getInt("id"), rs.getString("name"), rs.getString("password"), rs.getString("email"));
+				User u = new User(rs.getInt("id"), rs.getString("name"), rs.getString("password"),
+						rs.getString("email"));
 				projectUsers.add(u);
 			}
 			rs.close();
 			return projectUsers;
-		} catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
-	
+
 	private List<Message> getProjectMessages(int projectId) throws SQLException {
 		List<Message> projectMessages = new ArrayList<Message>();
-		
+
 		try (Connection con = DBController.getInstance().getConnection();
-		         PreparedStatement pstmt = con.prepareStatement(DBStatements.SELECT_PROJECT_MESSAGES_BY_PROJECT_ID)) {
+				PreparedStatement pstmt = con.prepareStatement(DBStatements.SELECT_PROJECT_MESSAGES_BY_PROJECT_ID)) {
 			pstmt.setInt(0, projectId);
-			
+
 			ResultSet rs = pstmt.executeQuery();
-			
+
 			while (rs.next()) {
 				User u = getUserById(rs.getInt("user_id"));
-				Message m = new Message(rs.getInt("id"), rs.getString("message"), DateUtils.toDate(rs.getString("created")), u);
+				Message m = new Message(rs.getInt("id"), rs.getString("message"),
+						DateUtils.toDate(rs.getString("created")), u);
 				projectMessages.add(m);
 			}
 			rs.close();
 			return projectMessages;
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return null;
-		
-		
+
 	}
 }
