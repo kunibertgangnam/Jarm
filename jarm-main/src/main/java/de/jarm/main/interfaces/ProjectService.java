ac2266fb475df3f59e5abfd3ef4869c113146c86
Andreas <andreas.kuehn@ingentis.com>
2020-02-04 10:44:57 +0100

    validierungsexceptions

--- jarm-main/src/main/java/de/jarm/main/interfaces/ProjectService.java
+++ jarm-main/src/main/java/de/jarm/main/interfaces/ProjectService.java
@@ -6,10 +6,12 @@ import de.jarm.main.data.Message;
 import de.jarm.main.data.Project;
 import de.jarm.main.data.ProjectToDo;
 import de.jarm.main.data.User;
+import de.jarm.main.utils.ValidierungsException;
 
 public interface ProjectService {
 
-	Project create(int projectid, String projectTitle, User projectOwner, LocalDate createDate);
+	Project create(int projectId, String projectTitle, User projectOwner, LocalDate createDate) throws ValidierungsException;
+	User create(int id, String name, String password, String email)throws ValidierungsException;
 	void writeMessage(int id, String message, User author);
 	void addSubscriber(User subscriber);
 	void removeSubscriber(User subscriber);
