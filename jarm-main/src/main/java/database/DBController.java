b3771fd36cf86c2b7d50359c64ec867e138a0eab
Jonas Maisl <kunibertgangnam@web.de>
2020-02-04 12:01:54 +0100

    reverted

--- jarm-main/src/main/java/database/DBController.java
+++ jarm-main/src/main/java/database/DBController.java
@@ -2,18 +2,15 @@ package database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DBController {
-<<<<<<< HEAD
 	
     private static final DBController dbcontroller = new DBController();
     private static final String DB_PATH = System.getProperty("user.home") + "/" + "jarmdb.db";
-=======
->>>>>>> branch 'Jarm-1-Datenbankmodell' of https://github.com/kunibertgangnam/Jarm
 
-<<<<<<< HEAD
     static {
         try {
             Class.forName("org.sqlite.JDBC");
@@ -47,69 +44,20 @@ public class DBController {
     	try (Connection con = getConnection(); 
     			Statement stmt = con.createStatement()){
 			
-=======
-	private static final DBController dbcontroller = new DBController();
-	// private static Connection connection;
-	private static final String DB_PATH = System.getProperty("user.home") + "/" + "jarmdb.db";
-
-	static {
-		try {
-			Class.forName("org.sqlite.JDBC");
-		} catch (ClassNotFoundException e) {
-			System.err.println("Fehler beim Laden des JDBC-Treibers");
-			e.printStackTrace();
-		}
-	}
-
-	private DBController() {
-		initDBConnection();
-	}
-
-	public static DBController getInstance() {
-		return dbcontroller;
-	}
-
-	public Connection getConnection() throws SQLException {
-		return DriverManager.getConnection("jdbc:sqlite:" + DB_PATH);
-	}
-
-	private void initDBConnection() {
-
-		createTables();
-
-//        Runtime.getRuntime().addShutdownHook(new Thread() {
-//            public void run() {
-//                try {
-//                    if (!connection.isClosed() && connection != null) {
-//                        connection.close();
-//                        if (connection.isClosed())
-//                            System.out.println("Connection to Database closed");
-//                    }
-//                } catch (SQLException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-	}
-
-	private void createTables() throws SQLException {
-		try {
-			Statement stmt = getConnection().createStatement();
->>>>>>> branch 'Jarm-1-Datenbankmodell' of https://github.com/kunibertgangnam/Jarm
 			stmt.execute(DBCreation.CREATE_USER_TABLE);
 			stmt.execute(DBCreation.CREATE_PROJECT_TABLE);
 			stmt.execute(DBCreation.CREATE_PROJECT_USER_TABLE);
 			stmt.execute(DBCreation.CREATE_PROJECT_TODO_TABLE);
 			stmt.execute(DBCreation.CREATE_PROJECT_TODO_USER_TABLE);
 			stmt.execute(DBCreation.CREATE_PROJECT_MESSAGE_TABLE);
-
+			
 			System.out.println("Tables created");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-	}
-
-	public static void main(String[] args) {
+    }
+    
+    public static void main(String[] args) {
 		System.out.println(DB_PATH);
 	}
 }
--- jarm-main/src/main/java/database/user/UserDAO.java
+++ jarm-main/src/main/java/database/user/UserDAO.java
@@ -7,9 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import database.DBController;
 import database.DBStatements;
