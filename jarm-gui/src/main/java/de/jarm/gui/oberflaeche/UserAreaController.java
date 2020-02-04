53e2ea8d3ff03ddfea36953e894b5a415e79e168
Jonas Maisl <kunibertgangnam@web.de>
2020-02-04 15:33:30 +0100

    Ordnerstruktur angepasst

--- jarm-gui/src/main/java/de/jarm/gui/oberflaeche/UserAreaController.java
+++ jarm-gui/src/main/java/de/jarm/gui/oberflaeche/UserAreaController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import de.jarm.gui.navi.Controller;
+import de.jarm.main.data.User;
 
 public class UserAreaController implements Controller {
 
@@ -11,11 +12,11 @@ public class UserAreaController implements Controller {
 	public String execute(HttpServletRequest request, HttpServletResponse response, StringBuffer message)
 			throws Exception {
 		
-//		Benutzer b = (Benutzer) request.getSession().getAttribute("user");
-//		if (b == null) {
-//			NotificationBuilder.createErrorNotification("Dafür musst du dich zunächst anmelden!", request);
-//			return "/error/error";
-//		}
+		User u = (User) request.getSession().getAttribute("user");
+		
+		if (u == null) {
+			return "/bv/login";
+		}
 		
 		return null;
 	}
