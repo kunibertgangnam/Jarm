72f43b983f11662fd5b26337095f7c9c2e7782de
Andreas <andreas.kuehn@ingentis.com>
2020-02-04 13:51:39 +0100

    Testbestände

--- jarm-main/src/main/java/de/jarm/main/utils/TestDatenBestand.java
+++ jarm-main/src/main/java/de/jarm/main/utils/TestDatenBestand.java
@@ -6,7 +6,13 @@ public class TestDatenBestand {
 	
 	public static void main(String[] args) {
 		try {
-			DataController.getInstance().getUserService().create(0, "Manni", "Manfred");
+			DataController.getInstance().getUserService().create(0, "Manni0", "Manfred");
+			DataController.getInstance().getUserService().create(1, "Manni1", "Manfred");
+			DataController.getInstance().getUserService().create(2, "Manni2", "Manfred");
+			DataController.getInstance().getUserService().create(3, "Manni3", "Manfred");
+			DataController.getInstance().getUserService().create(4, "Manni4", "Manfred");
+			DataController.getInstance().getUserService().create(5, "Manni5", "Manfred");
+			DataController.getInstance().getUserService().create(6, "Manni6", "Manfred");
 		} catch (ValidierungsException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
