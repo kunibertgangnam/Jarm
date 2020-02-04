b317620ee8d1dd1a1704fddd046417deab1ff6da
Ronja3007 <ronja.galler@ingentis.com>
2020-02-04 14:42:45 +0100

    kleine Sachen

--- jarm-gui/src/main/java/bv/gui/ForumController.java
+++ jarm-gui/src/main/java/bv/gui/ForumController.java
@@ -1,5 +1,10 @@
 package bv.gui;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -16,17 +21,20 @@ public class ForumController implements Controller {
 			nachrichtPruefen(nachricht);
 			request.setAttribute("nachricht", null);
 			nachrichtEinfuegen(nachricht, request);
-			nachrichtenAusgeben();
+			request.getSession().setAttribute("nachrichten", nachrichtenAusgeben());
 		}
 		return null;
 	}
 
-	private void nachrichtenAusgeben() {
-		//DataController.getInstance().getProjectService()
+	private Map<String, String> nachrichtenAusgeben() {
+		Map<String, String> nachrichtenMitName = new HashMap<String, String>();
+		List <Object> ausgelesen = new ArrayList<>();
+		//noch net fertig
+		return nachrichtenMitName;
 	}
 
 	private void nachrichtEinfuegen(String nachricht, HttpServletRequest request) {
-		DataController.getInstance().getProjectService().writeMessage(request.getSession().getAttribute("projektName"), id, nachricht, request.getSession().getAttribute("eingeloggt"));
+		DataController.getInstance().getProjectService().writeMessage(request.getSession().getAttribute("projektName"), nachricht, request.getSession().getAttribute("eingeloggt"));
 	}
 
 	private void nachrichtPruefen(String nachricht) throws Exception {
