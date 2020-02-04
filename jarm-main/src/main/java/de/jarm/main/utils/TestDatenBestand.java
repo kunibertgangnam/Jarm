4e89ce8017296a43666b77c7b6d4aad27edce27b
Andreas <andreas.kuehn@ingentis.com>
2020-02-04 14:04:47 +0100

    getUser methode

--- jarm-main/src/main/java/de/jarm/main/utils/TestDatenBestand.java
+++ jarm-main/src/main/java/de/jarm/main/utils/TestDatenBestand.java
@@ -6,15 +6,14 @@ public class TestDatenBestand {
 	
 	public static void main(String[] args) {
 		try {
-			DataController.getInstance().getUserService().create(0, "Manni0", "Manfred");
-			DataController.getInstance().getUserService().create(1, "Manni1", "Manfred");
-			DataController.getInstance().getUserService().create(2, "Manni2", "Manfred");
-			DataController.getInstance().getUserService().create(3, "Manni3", "Manfred");
-			DataController.getInstance().getUserService().create(4, "Manni4", "Manfred");
-			DataController.getInstance().getUserService().create(5, "Manni5", "Manfred");
-			DataController.getInstance().getUserService().create(6, "Manni6", "Manfred");
+			DataController.getInstance().getUserService().create("Manni0", "Manfred");
+			DataController.getInstance().getUserService().create("Manni1", "Manfred");
+			DataController.getInstance().getUserService().create("Manni2", "Manfred");
+			DataController.getInstance().getUserService().create("Manni3", "Manfred");
+			DataController.getInstance().getUserService().create("Manni4", "Manfred");
+			DataController.getInstance().getUserService().create("Manni5", "Manfred");
+			DataController.getInstance().getUserService().create("Manni6", "Manfred");
 		} catch (ValidierungsException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
