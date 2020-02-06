9b461cf5f7fa99d3f17d2ce22061e91369dd2187
Andreas <andreas.kuehn@ingentis.com>
2020-02-06 09:43:16 +0100

    versuch projektid

--- jarm-gui/src/main/java/de/jarm/gui/oberflaeche/ProjectController.java
+++ jarm-gui/src/main/java/de/jarm/gui/oberflaeche/ProjectController.java
@@ -17,27 +17,33 @@ public class ProjectController implements Controller {
 	public String execute(HttpServletRequest request, HttpServletResponse response, StringBuffer message)
 			throws Exception {
 		
-		int projectId = new Integer(request.getParameter("id"));
+		
+		
 		
 		try {
-			Project p = DataController.getInstance().getProjectService().getProjectById(projectId);
-			request.setAttribute("currentProject", p);
-			List<Message> messagesList = p.getMessages();
-			if(request.getMethod().equals("POST")) {
-				if(!request.getParameter("message").equals("")){
-					messagesList.add(new Message(request.getParameter("message"), (User)request.getSession().getAttribute("user")));
-					p.setMessages(messagesList);
+			Object id = request.getAttribute("id");
+			int projectId;
+			
+			if(id!=null) {
+				projectId = (int)id;
+				Project p = DataController.getInstance().getProjectService().getProjectById(projectId);
+				request.setAttribute("currentProject", p);
+				List<Message> messagesList = p.getMessages();
+				if(request.getMethod().equals("POST")) {
+					if(!request.getParameter("message").equals("")){
+						messagesList.add(new Message(request.getParameter("message"), (User)request.getSession().getAttribute("user")));
+						p.setMessages(messagesList);
+					}
 				}
-			}
-			
-			String messages = "";
-			for(int i = messagesList.size()-1; i > messagesList.size()-51 && i > 0; i--) {
-				messages += "<font style=\"font-weight=bold\">"+ messagesList.get(i).getAuthor().getName()+ "</font><br>" + 
-				messagesList.get(i).getMessage()+ "<br><br>";
-			}
-			
-			request.setAttribute("nachrichten", messages);
-			
+				
+				String messages = "";
+				for(int i = messagesList.size()-1; i > messagesList.size()-51 && i > 0; i--) {
+					messages += "<font style=\"font-weight=bold\">"+ messagesList.get(i).getAuthor().getName()+ "</font><br>" + 
+					messagesList.get(i).getMessage()+ "<br><br>";
+				}
+				
+				request.setAttribute("nachrichten", messages);
+			}	
 		} catch(Exception e) {
 			message.append(e.getMessage());
 		}
