52c4a09df33d935c986d5fb314360aa72c94ff26
Andreas <andreas.kuehn@ingentis.com>
2020-02-06 09:15:20 +0100

    Nachrichten anzeigen

--- jarm-gui/src/main/java/de/jarm/gui/oberflaeche/ProjectController.java
+++ jarm-gui/src/main/java/de/jarm/gui/oberflaeche/ProjectController.java
@@ -1,10 +1,13 @@
 package de.jarm.gui.oberflaeche;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import de.jarm.gui.navi.Controller;
 import de.jarm.main.data.DataController;
+import de.jarm.main.data.Message;
 import de.jarm.main.data.Project;
 
 public class ProjectController implements Controller {
@@ -18,9 +21,20 @@ public class ProjectController implements Controller {
 		try {
 			Project p = DataController.getInstance().getProjectService().getProjectById(projectId);
 			request.setAttribute("currentProject", p);
+			
+			String messages = "";
+			List<Message> messagesList = p.getMessages();
+			for(int i = messagesList.size()-1; i > messagesList.size()-51 && i > 0; i--) {
+				messages += "<font style=\"font-weight=bold\">"+ messagesList.get(i).getAuthor().getName()+ "</font><br>" + 
+				messagesList.get(i).getMessage()+ "<br><br>";
+			}
+			
+			request.setAttribute("nachrichten", messages);
+			
 		} catch(Exception e) {
 			message.append(e.getMessage());
 		}
+		
 		
 		return null;
 	}
