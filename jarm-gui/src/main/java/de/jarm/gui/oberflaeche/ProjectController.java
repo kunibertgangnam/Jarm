0ad4aedcd8b2ccf8a9bf301f998a5cc44464b7a8
Friedrich Kiltz <f@kiltz.de>
2020-02-06 10:29:26 +0100

    mit Import

--- jarm-gui/src/main/java/de/jarm/gui/oberflaeche/ProjectController.java
+++ jarm-gui/src/main/java/de/jarm/gui/oberflaeche/ProjectController.java
@@ -1,5 +1,7 @@
 package de.jarm.gui.oberflaeche;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,35 +9,33 @@ import de.jarm.gui.navi.Controller;
 import de.jarm.main.data.DataController;
 import de.jarm.main.data.Message;
 import de.jarm.main.data.Project;
-import de.jarm.main.data.User;
 
 public class ProjectController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response, StringBuffer message)
 			throws Exception {
-		
+
 		try {
 			Object id = request.getAttribute("id");
 			int projectId;
-			if(id!=null) {
-				projectId = (int)id;
+			if (id != null) {
+				projectId = (int) id;
 				Project p = DataController.getInstance().getProjectService().getProjectById(projectId);
-				
+
 				List<Message> messagesList = p.getMessages();
 				String messages = "";
-				for(int i = messagesList.size()-1; i > messagesList.size()-51 && i > 0; i--) {
-					messages += "<font style=\"font-weight=bold\">"+ messagesList.get(i).getAuthor().getName()+ "</font><br>" + 
-					messagesList.get(i).getMessage()+ "<br><br>";
+				for (int i = messagesList.size() - 1; i > messagesList.size() - 51 && i > 0; i--) {
+					messages += "<font style=\"font-weight=bold\">" + messagesList.get(i).getAuthor().getName()
+							+ "</font><br>" + messagesList.get(i).getMessage() + "<br><br>";
+				}
+
+				request.setAttribute("nachrichten", messages);
 			}
-			
-			request.setAttribute("nachrichten", messages);
-			}	
-		} catch(Exception e) {
+		} catch (Exception e) {
 			message.append(e.getMessage());
 		}
-		
-		
+
 		return null;
 	}
 
